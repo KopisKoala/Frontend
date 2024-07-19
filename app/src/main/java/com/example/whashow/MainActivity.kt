2a2fb19f6a5d41fdc6f631e07310d32d1af8c843
HwangSeo <sadclara0830@naver.com>
2024-07-19 23:14:04 +0900

    앱바 구현

--- app/src/main/java/com/example/whashow/MainActivity.kt
+++ app/src/main/java/com/example/whashow/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.whashow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.whashow.databinding.ActivityMainBinding
 import com.example.whashow.ui.home.HomeFragment
 import com.example.whashow.ui.mypage.MypageFragment
 import com.example.whashow.ui.pairing.PairingFragment
+import com.example.whashow.ui.recommand.GenreFragment
 import com.example.whashow.ui.recommand.RecommandResultFragment
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +25,59 @@ class MainActivity : AppCompatActivity() {
         showInit()
         initBottomNav()
 
+        setSupportActionBar(binding.toolbar)
+
+        // Disable displaying the title in the Toolbar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.btnBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager
+                .popBackStack()
+        }
+        else {
+            super.onBackPressed()
+        }
+
+    }
+
+    fun ShowBack() {
+        binding.btnBack.visibility = View.VISIBLE
+
+        binding.toolbarLogo.visibility = View.GONE
+        binding.btnMainSearch.visibility = View.GONE
+        binding.mainTitle.visibility = View.GONE
+        binding.backTitle.visibility = View.GONE
+    }
+    fun ShowBackandTitle() {
+        binding.btnBack.visibility = View.VISIBLE
+        binding.backTitle.visibility = View.VISIBLE
+
+        binding.toolbarLogo.visibility = View.GONE
+        binding.btnMainSearch.visibility = View.GONE
+        binding.mainTitle.visibility = View.GONE
+    }
+    fun ShowTitle() {
+        binding.mainTitle.visibility = View.VISIBLE
+
+        binding.btnBack.visibility = View.GONE
+        binding.backTitle.visibility = View.GONE
+        binding.toolbarLogo.visibility = View.GONE
+        binding.btnMainSearch.visibility = View.GONE
+
+    }
+    fun ShowLogoAndSearch(){
+        binding.toolbarLogo.visibility = View.VISIBLE
+        binding.btnMainSearch.visibility = View.VISIBLE
+
+        binding.mainTitle.visibility = View.GONE
+        binding.btnBack.visibility = View.GONE
+        binding.backTitle.visibility = View.GONE
     }
     private fun showInit() {
         val transaction = supportFragmentManager.beginTransaction()
@@ -39,7 +95,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.navigation_recommand -> {
-                    changeFragment(RecommandResultFragment())
+                    addFragment(GenreFragment())
                 }
 
                 R.id.navigation_pairing -> {
@@ -71,4 +127,5 @@ class MainActivity : AppCompatActivity() {
             }
 
     }
+
 }
