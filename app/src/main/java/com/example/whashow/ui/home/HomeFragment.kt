df5008717b3a51863f3519c5120f511d7b013b1e
Lee-EunSeok <lanmecan@naver.com>
2024-07-25 00:32:41 +0900

    dd

--- app/src/main/java/com/example/whashow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/whashow/ui/home/HomeFragment.kt
@@ -11,18 +11,18 @@ import com.example.whashow.base.BaseFragment
 import com.example.whashow.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    override fun initStartView() {
-        super.initStartView()
-        (activity as MainActivity).ShowLogoAndSearch()
-        (activity as MainActivity).binding.navigationMain.visibility=View.VISIBLE
-    }
+        override fun initStartView() {
+            super.initStartView()
+            (activity as MainActivity).ShowLogoAndSearch()
+            (activity as MainActivity).binding.navigationMain.visibility=View.VISIBLE
+        }
 
-    override fun initDataBinding() {
-        super.initDataBinding()
-    }
+        override fun initDataBinding() {
+            super.initDataBinding()
+        }
 
-    override fun initAfterBinding() {
-        super.initAfterBinding()
+        override fun initAfterBinding() {
+            super.initAfterBinding()
 
-    }
+        }
 }
