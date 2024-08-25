1aa324708655dc35947a1c31e3b7184520c25139
HwangSeo <sadclara0830@naver.com>
2024-08-25 21:37:04 +0900

    배우 찜 서버 연결, 배우 들어오면 테스트 필요

--- app/src/main/java/com/example/whashow/api/MypageService.kt
+++ app/src/main/java/com/example/whashow/api/MypageService.kt
@@ -3,6 +3,7 @@ package com.example.whashow.api
 import com.example.whashow.data.AddProfile
 import com.example.whashow.data.CalendarDayReview
 import com.example.whashow.data.CalendarMonth
+import com.example.whashow.data.DeleteMyActor
 import com.example.whashow.data.Goods
 import com.example.whashow.data.Info
 import com.example.whashow.data.KakaoLogin
@@ -15,6 +16,7 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
@@ -42,5 +44,8 @@ interface MypageService {
     fun getGoodsList(@Header("Authorization") accessToken:String): Call<Goods>
     @GET("/favorite/actor/list")
     fun getMyActorList(@Header("Authorization") accessToken:String,@Query("ScrollPosition") position: Int,@Query("FetchSize") size: Int): Call<MyActor>
+    @DELETE("/favorite/actor/delete/{actorId}")
+    fun deleteActor(@Header("Authorization") accessToken:String,@Path("actorId") id: Int): Call<DeleteMyActor>
+
 
 }
--- app/src/main/java/com/example/whashow/ui/mypage/MyActorAdapter.kt
+++ app/src/main/java/com/example/whashow/ui/mypage/MyActorAdapter.kt
@@ -2,16 +2,24 @@ package com.example.whashow.ui.mypage
 
 import android.annotation.SuppressLint
 import android.graphics.Paint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.whashow.R
+import com.example.whashow.apiManager.ApiManager
+import com.example.whashow.data.DeleteMyActor
 import com.example.whashow.data.FavoriteActorResDto
 import com.example.whashow.data.GoodsX
+import com.example.whashow.data.MyActor
 import com.example.whashow.databinding.GoodsGridItemBinding
 import com.example.whashow.databinding.MyActorListItemBinding
+import com.example.whashow.login.LocalDataSource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyActorAdapter (var list: ArrayList<FavoriteActorResDto>): RecyclerView.Adapter<MyActorAdapter.ActorViewHolder>() {
 
@@ -49,6 +57,34 @@ class MyActorAdapter (var list: ArrayList<FavoriteActorResDto>): RecyclerView.Ad
             .into(holder.profile)
         holder.delete.setOnClickListener {
             removeItem(position)
+            /*//선택되지 않은 경우
+            val Call: Call<DeleteMyActor> =
+                ApiManager.mypageService.deleteActor(
+                    "Bearer " + LocalDataSource.getAccessToken(),list[position].actorId
+                )
+            // 비동기적으로 요청 수행
+            Call.enqueue(object : Callback<DeleteMyActor> {
+                override fun onResponse(
+                    call: Call<DeleteMyActor>,
+                    response: Response<DeleteMyActor>
+                ) {
+                    if (response.isSuccessful) {
+
+                        Log.d("내 배우 서버", response.body()?.result.toString())
+
+                    } else {
+                        // 서버에서 오류 응답을 받은 경우 처리
+                        Log.d("내 배우 서버", response.toString())
+                    }
+
+                }
+
+                override fun onFailure(call: Call<DeleteMyActor>, t: Throwable) {
+                    // 통신 실패 처리
+                    Log.d("내 배우 서버", t.message.toString())
+                }
+
+            })*/
         }
         if (editClick){
             holder.delete.visibility= View.VISIBLE
