21f4fa2af0959b07e11879901dad4550ed2bee2c
HwangSeo <sadclara0830@naver.com>
2024-08-29 08:11:55 +0900

    공연 리뷰 작성 서버 연결

--- app/src/main/java/com/example/whashow/ui/home/DetailReviewFragment.kt
+++ app/src/main/java/com/example/whashow/ui/home/DetailReviewFragment.kt
@@ -1,25 +1,46 @@
 package com.example.whashow.ui.home
 
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whashow.MainActivity
 import com.example.whashow.R
+import com.example.whashow.apiManager.ApiManager
 import com.example.whashow.base.BaseFragment
+import com.example.whashow.data.PairReview
+import com.example.whashow.data.Review
 import com.example.whashow.databinding.FragmentDetailReviewBinding
+import com.example.whashow.login.LocalDataSource
+import com.example.whashow.ui.home.Adapter.PerformanceReviewAdapter
+import com.example.whashow.ui.pairing.PairingViewPagerAdapter
+import com.example.whashow.ui.pairing.ReviewAdapter
+import com.example.whashow.ui.recommand.SortResultSpinnerAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailReviewFragment: BaseFragment<FragmentDetailReviewBinding>(R.layout.fragment_detail_review) {
 
     private val viewModel: PerformanceDetailViewModel by viewModels()
-    private var perfId: Int? = null
+    //private var perfId: Int? = null
+    private val _perfId= MutableLiveData<Int>()
+    val perId: LiveData<Int>
+        get()=_perfId
 
+    private lateinit var reviewAdapter: PerformanceReviewAdapter
 
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).binding.navigationMain.visibility = View.GONE
-
-        perfId = arguments?.getInt("perfId")
-        perfId?.let {
+        _perfId.value = arguments?.getInt("perfId")
+        _perfId.value?.let {
+            Log.d("리뷰 목록 조회",it.toString())
             viewModel.fetchPerformanceData(it)
         }
     }
@@ -27,6 +48,14 @@ class DetailReviewFragment: BaseFragment<FragmentDetailReviewBinding>(R.layout.f
         super.initDataBinding()
         (activity as MainActivity).binding.navigationMain.visibility = View.GONE
 
+
+        _perfId.observe(requireActivity(), Observer { id ->
+            reviewAdapter = PerformanceReviewAdapter(arrayListOf(), id)
+        })
+
+        binding.reviewRv.adapter = reviewAdapter
+        binding.reviewRv.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
         viewModel.hashtags.observe(viewLifecycleOwner, Observer { hashtags ->
             if (hashtags.isNotEmpty()) {
@@ -40,6 +69,36 @@ class DetailReviewFragment: BaseFragment<FragmentDetailReviewBinding>(R.layout.f
             binding.ratingBar.rating = rating
             binding.ratingNum.text = rating.toString()
         })
+
+
+        val sortTextList = listOf(
+            "최신 순",
+            "좋아요 순",
+            "별점 낮은 순",
+            "별점 높은 순"
+        )
+        // spinnerRecommandSpace를 레이아웃 파일에서 가져옴
+        val spinnerSortResult: Spinner = binding.spinnerReviewSort
+        spinnerSortResult.adapter= SortResultSpinnerAdapter(requireContext(),R.layout.item_spinner_sort_result,sortTextList)
+        spinnerSortResult.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val value = spinnerSortResult.getItemAtPosition(p2).toString()
+                _perfId.observe(requireActivity(), Observer { id ->
+                    Log.d("리뷰 목록 조회", id.toString())
+                    viewModel.fetchPerformanceReview(id, p2)
+                    viewModel.reviewList.observe(viewLifecycleOwner, Observer { list ->
+                        reviewAdapter.updateReviews(list)
+                        binding.reviewNum.text=list.size.toString()
+                    })
+                })
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
 
 
     }
