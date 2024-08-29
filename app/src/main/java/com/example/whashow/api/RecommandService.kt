9c07b14296fefdf13681d0ed8be7436734433d3a
HwangSeo <sadclara0830@naver.com>
2024-08-29 09:54:17 +0900

    추천 서비스 생성, 공연 페어 추천 api 호출 함수 작성

---
+++ app/src/main/java/com/example/whashow/api/RecommandService.kt
@@ -0,0 +1,29 @@
+package com.example.whashow.api
+
+import com.example.whashow.data.RecommandPairList
+import com.example.whashow.data.RecommandPerformanceList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface RecommandService {
+    @GET("/performance/recommend")
+    fun getPerformanceList(
+        @Header("Authorization") accessToken: String,
+        @Query("startYear") year:Int,
+        @Query("startMonth") startMonth:Int,
+        @Query("startDate") startDate:Int,
+        @Query("endYear") endYear:Int,
+        @Query("endMonth") endMonth:Int,
+        @Query("endDate") endDate:Int,
+        @Query("location") location:Int,
+        @Query("minPrice") minPrice:Int,
+        @Query("maxPrice") maxPrice:Int,
+    ) : Call<RecommandPerformanceList>
+
+    @GET("/pair/recommend")
+    fun getPairList(
+        @Header("Authorization") accessToken: String,
+    ): Call<RecommandPairList>
+}
